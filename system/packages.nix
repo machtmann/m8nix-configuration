09dc0188f45f9febab470159b1bfd22012c061a2
m8man <moritz.achtmann@gmail.com>
2025-07-27 12:06:05 +0200

    Add required libs for steam

--- system/packages.nix
+++ system/packages.nix
@@ -61,6 +61,34 @@ in {
       dedicatedServer.openFirewall = true;
       gamescopeSession.enable = true;
       remotePlay.openFirewall = true;
+      package = pkgs.steam.override {
+        extraLibraries = pkgs: with pkgs; [
+          alsa-lib
+          keyutils
+          libkrb5
+          libpng
+          libpulseaudio
+          libvorbis
+          libxkbcommon
+          pipewire
+          source-han-sans
+          source-han-serif
+          source-sans
+          source-serif
+          stdenv.cc.cc.lib
+          udev
+          vulkan-loader
+          wayland
+          xorg.libX11
+          xorg.libXScrnSaver
+          xorg.libXcursor
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXrandr
+        ];
+      };
     };
     thunar = {
       enable = true;
@@ -69,5 +97,6 @@ in {
         thunar-volman
       ];
     };
+    xwayland.enable = true;
   };
 }
