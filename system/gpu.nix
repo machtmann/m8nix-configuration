4d6a8204055bc93c617c3d5aee2d5a7a949fefe0
m8man <moritz.achtmann@gmail.com>
2025-07-31 00:54:43 +0200

    Fix: nvidia-offload

--- system/gpu.nix
+++ system/gpu.nix
@@ -1,16 +1,31 @@
-{ config, lib, pkgs, ... }:{
+{ config, pkgs, ... }:
 
-  services = {
-    xserver = {
-      enable = true;
-      videoDrivers = [ "modesetting" "nvidia" ];
-    };
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in
+{
+  nixpkgs.config.nvidia.acceptLicense = true;
+
+  environment.systemPackages = [ nvidia-offload ];
+
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "nvidia" ];
   };
 
   hardware = {
     graphics.enable = true;
     nvidia = {
-      open = true;
+      modesetting.enable = true;
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
+      powerManagement.enable = true;
       prime = {
         offload.enable = true;
         intelBusId = "PCI:0:2:0";
